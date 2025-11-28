fd1eb17125b860c2c54dd454ec45a9256029e986
Avishkar Kabadi <155943160+Avishkar-Kabadi@users.noreply.github.com>
2025-11-28 09:12:35 +0530

    Secure Firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,19 +1,28 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const { VITE_FIREBASE_API_KEY,
+    VITE_FIREBASE_AUTH_DOMAIN,
+    VITE_FIREBASE_PROJECT_ID,
+    VITE_FIREBASE_STORAGE_BUCKET,
+    VITE_FIREBASE_MESSAGING_SENDER_ID,
+    VITE_FIREBASE_APP_ID,
+    VITE_FIREBASE_MESUREMENT_ID
+} = import.meta.env
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAtI3MH2y6eyahQNMRnCJn3VloW65r9ub8",
-    authDomain: "moviesgpt-13f90.firebaseapp.com",
-    projectId: "moviesgpt-13f90",
-    storageBucket: "moviesgpt-13f90.firebasestorage.app",
-    messagingSenderId: "648157989240",
-    appId: "1:648157989240:web:1b2354e261915b810fa08d",
-    measurementId: "G-14LHJQ3T69"
+    apiKey: VITE_FIREBASE_API_KEY,
+    authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: VITE_FIREBASE_PROJECT_ID,
+    storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: VITE_FIREBASE_APP_ID,
+    measurementId: VITE_FIREBASE_MESUREMENT_ID
 };
 
 // Initialize Firebase
